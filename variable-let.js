2b0391667686fc65b91d119bf5caeb7a17e30f57
Muhammad Fillah Alfatih <m.fiilah.a.f@gmail.com>
2021-07-18 10:49:04 +0700

    Create let variable

---
+++ variable-let.js
@@ -0,0 +1,25 @@
+
+let namaDepan;
+let namaBelakang;
+let namaLengkap;
+
+namaDepan = "Fillah";
+namaBelakang = "Alfatih";
+namaPanjang = namaDepan + " " + namaLengkap;
+
+document.writeln(`<h1>Hai, saya ${namaPanjang}</h1>`);
+document.writeln(`<h2>Selamat datang, ${namaDepan}</h2>`);
+
+let myNumber;
+let otherNumber;
+let addition;
+let subtract;
+
+myNumber = 32;
+otherNumber = 10;
+
+addition = myNumber + otherNumber;
+subtract = myNumber - otherNumber;
+
+document.writeln(`<p>Addition = ${addition}</p>`)
+document.writeln(`<p>Addition = ${subtract}</p>`)
