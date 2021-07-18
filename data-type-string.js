3aa6a12d55edce92885949475f6f2442062d9bb1
Muhammad Fillah Alfatih <m.fiilah.a.f@gmail.com>
2021-07-18 10:48:01 +0700

    Create data type string

---
+++ data-type-string.js
@@ -0,0 +1,12 @@
+
+document.writeln("Nama : " + "Muhammad Fillah Alfatih");
+document.writeln(`</br>`);
+document.writeln("Pekerjaan : " + "Fullstack Engineer")
+
+// Escape Sequence
+document.write("<textarea cols='50' rows='25'>")
+document.write("Muhammad Fillah Alfatih\n")
+document.write("\\Home \\Projects \\Articles\n")
+document.write("\"Fullstack Engineer\"\n")
+document.write('\'Belajar Javascript\'\n')
+document.write("</textarea>")
