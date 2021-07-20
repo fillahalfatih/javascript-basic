da6ca0cb103fd3a3daa773a482879d1b8499d868
Muhammad Fillah Alfatih <m.fiilah.a.f@gmail.com>
2021-07-20 07:40:24 +0700

    String template - modern Javascript

---
+++ string-template.js
@@ -0,0 +1,24 @@
+
+const name = "Muhammad Fillah Alfatih";
+const template = `Name : ${name}`;
+
+console.log(template);
+
+const firstName = "Fillah";
+const lasttName = "Alfatih";
+const fullName = `${firstName} ${lasttName}`;
+
+console.log(`Selamat Datang, ${firstName}`);
+
+// Expression
+const value = 90;
+const template2 = `Name : ${name} \nResult : ${value > 80}`;
+
+console.log(template2);
+
+// Multiline --> use ``
+const multiLineStr = `Nama saya Fillah,
+Sekarang lagi di depan laptop,
+Suka denger lagu Jung Seung Hwan`;
+
+document.writeln(`<pre>${multiLineStr}</pre>`)
