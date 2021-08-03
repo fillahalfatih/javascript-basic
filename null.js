7b54101b7c649c6c90b8b5bb372d277b21b6af19
Muhammad Fillah Alfatih <m.fiilah.a.f@gmail.com>
2021-08-03 19:09:13 +0700

    Null

---
+++ null.js
@@ -0,0 +1,10 @@
+
+let firstName = null;
+
+if (firstName === undefined) {
+    alert("firstName is undefined");
+} else if (firstName === null) {
+    alert("firstName is null");
+} else {
+    alert(`Hey, ${firstName}`);
+}
