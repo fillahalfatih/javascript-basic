babc8ef987f577c87942c0091f6c8b132160c1fb
Muhammad Fillah Alfatih <m.fiilah.a.f@gmail.com>
2021-10-30 18:19:02 +0700

    Add typeof operation

---
+++ typeof.js
@@ -0,0 +1,15 @@
+
+// typeof
+const data = [];
+const typeData = typeof data;
+
+alert(`it's ${typeData}`)
+
+/*
+  null = object
+  {} = object
+  [] = object
+  true = boolean
+  100 = number
+  "..." = string
+*/
